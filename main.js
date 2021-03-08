c5e0332b709cf24b143854038a071e2763088126
Ilya Zhurba <i.n.zhurba@gmail.com>
2021-03-07 22:48:45 -0700

    First commit. Added time to title and body

---
+++ main.js
@@ -0,0 +1,19 @@
+function updateClock() {
+    var now = new Date(), // current date
+        months = ['January', 'February', '...']; // you get the idea
+        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
+
+        // a cleaner way than string concatenation
+        date = [now.getDate(), 
+                months[now.getMonth()],
+                now.getFullYear()].join(' ');
+
+    // set the content of the element with the ID time to the formatted string
+  document.getElementById('time').innerHTML = [date, time].join(' / ');
+  document.title = time;
+}
+setInterval(updateClock, 1000);
+
+
+
+
